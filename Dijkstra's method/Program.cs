2219e4bc805fcaa7f821116902e7b1aac8f2e8ea
Bobarik31p <kbobarik@mail.ru>
2025-02-17 23:04:59 +0300

    Добавьте файлы проекта.

---
+++ Dijkstra's method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra_s_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            MethodsClass obj = new MethodsClass("graph.txt");
+            obj.Dijkstra();
+
+            
+            Console.ReadKey();
+        }
+    }
+}
